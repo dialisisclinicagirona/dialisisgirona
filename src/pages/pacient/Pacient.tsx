718c55523f7c4aead42e90945d5bfb0dc7b86cdb
afont_disgrafic <afont@disgrafic.com>
2025-04-06 22:20:00 +0200

    llista pacients tipada

--- src/pages/pacient/Pacient.tsx
+++ src/pages/pacient/Pacient.tsx
@@ -4,28 +4,29 @@ import { Database } from '../../types/supabase';
 import { useNavigate } from 'react-router-dom';
 
 type Pacient = Database['public']['Tables']['pacients']['Row'];
+type PacientLlista = Pick<Database['public']['Tables']['pacients']['Row'], 'id' | 'nom'>;
 
 const Pacient = () => {
   const navigate = useNavigate();
-  const [pacients, setPacients] = useState<Pacient[]>([]);
+  const [pacients, setPacients] = useState<PacientLlista[]>([]);
+  let dadescarregades = false;
+  
 
   useEffect(() => {
-    
-
     const carregarPacients = async () => {
 
       // 1. Verifica l'usuari
       const { data: { user }, error: authError } = await supabase.auth.getUser();
-      console.log("Usuari:", user, "Error:", authError);
+      //console.log("Usuari:", user, "Error:", authError);
 
+      // 
       if(user && authError == null){
-
-        if(pacients.length == 0){
-
+        if(pacients.length == 0 && !dadescarregades){
+          dadescarregades = true;
           const { data, error } = await supabase
             .from('pacients')
-            .select('*');
-          
+            .select('id, nom');
+            
           console.log(data);
           if (!error) setPacients(data);
         }
