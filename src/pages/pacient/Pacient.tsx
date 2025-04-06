a171fd3083c05de810805dd288749619640dad85
afont_disgrafic <afont@disgrafic.com>
2025-04-06 18:40:43 +0200

    llista de pacients

--- src/pages/pacient/Pacient.tsx
+++ src/pages/pacient/Pacient.tsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { supabase } from "../../lib/supabaseClient";
+import { Database } from '../../types/supabase';
+import { useNavigate } from 'react-router-dom';
+
+type Pacient = Database['public']['Tables']['pacients']['Row'];
 
 const Pacient = () => {
+  const navigate = useNavigate();
+  const [pacients, setPacients] = useState<Pacient[]>([]);
+
+  useEffect(() => {
+    
+
+    const carregarPacients = async () => {
+
+      // 1. Verifica l'usuari
+      const { data: { user }, error: authError } = await supabase.auth.getUser();
+      console.log("Usuari:", user, "Error:", authError);
+
+      if(user && authError == null){
+
+        if(pacients.length == 0){
+
+          const { data, error } = await supabase
+            .from('pacients')
+            .select('*');
+          
+          console.log(data);
+          if (!error) setPacients(data);
+        }
+    }
+      else{
+        navigate("/");
+      }
+    };
+    carregarPacients();
+  }, []);
+
   return (
     <div>
-      <h1>Fitxa d'un pacient</h1>
+      <ul>
+      {pacients.map(pacient => (
+        <li key={pacient.id}>{pacient.nom}</li>
+      ))}
+    </ul>
       
     </div>
   );
