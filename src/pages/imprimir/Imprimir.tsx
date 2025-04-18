96e7ae8af495ebf643d1a53b2b626c46377043c0
dialisisclinicagirona <dialisisclinicagirona@gmail.com>
2025-04-18 15:38:36 +0200

    fix use params

--- src/pages/imprimir/Imprimir.tsx
+++ src/pages/imprimir/Imprimir.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import { supabase } from "../../lib/supabaseClient";
 import './ImprimirPacient.css';
 import { DadaPacient } from '../../types/supabase';
